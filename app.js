c37745604e6298b1fc27bfa798df8b25e7b2f086
Aaron Zahl <43203730+Zahlsky@users.noreply.github.com>
2022-07-20 19:53:35 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -12,10 +12,19 @@ function init() {
   const charAliClass = 'alien'
   const charLaserClass = 'laser'
   const charExplosionClass = 'explosion'
+  const charMissileClass = 'enemyMissile'
   let aliens = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 61, 62, 63, 64, 65, 66, 67, 68, 82, 87]
-  let aliensTimer
   let direction = 1
-  let score = document.querySelector('.score')
+  let scoreDisplay = document.querySelector('.results')
+  let score = 0
+  let deadAliens = []
+  let level = document.querySelector('.levelNum')
+  const startButton = document.querySelector('.start')
+  let aliensTimer
+  let laserTimer
+  let randomFireTimer
+  let fireDown
+  let missileTimer
 
 
 
@@ -27,21 +36,34 @@ function init() {
       grid.appendChild(cell)
     }
 
+  }
+  function pageLoad() {
+    clearInterval(aliensTimer)
+  }
+
+  function start() {
+    console.log('start')
+    scoreDisplay.innerHTML = 0
     addShooter(currentPosition)
     addAliens(aliensStartingPosition)
+    aliensTimer = setInterval(moveAliens, 400)
+    fireMissiles()
+    randomFire()
   }
+
 
   // add Aliens array to the cells 
 
   function addAliens() {
 
+    //re-add aliens only if not included in array
+
     for (let i = 0; i < aliens.length; i++) {
-      const element = aliens[i]
-      cells[element].classList.add(charAliClass)
+      if (!deadAliens.includes(i)) {
+        cells[aliens[i]].classList.add(charAliClass)
+      }
     }
-
   }
-
   // remove Aliens from the cells
   function removeAliens() {
 
@@ -50,6 +72,8 @@ function init() {
       cells[element].classList.remove(charAliClass)
     }
   }
+
+
 
   // move and remove to create movement add and remove (left is -1, right is 1)
 
@@ -80,18 +104,23 @@ function init() {
     addAliens()
 
     if (cells[currentPosition].classList.contains(charAliClass, charClass)) {
-      cells[currentPosition].classList.add(charExplosionClass)
       cells[currentPosition].classList.remove(charAliClass, charClass)
+      cells[currentPosition].classList.add(charExplosionClass)
 
       console.log('game over')
-      score.innerHTML = 'GAME OVER'
+      scoreDisplay.innerHTML = 'GAME OVER'
       clearInterval(aliensTimer)
+      clearInterval(laserTimer)
+    }
 
-
+    for (let i = 0; i < aliens.length; i++) {
+      if (aliens[i] > (cells.length)) {
+        scoreDisplay.innerHTML = 'GAME OVER'
+        clearInterval(laserTimer)
+        clearInterval(aliensTimer)
+      }
     }
   }
-
-  aliensTimer = setInterval(moveAliens, 800)
 
 
 
@@ -121,6 +150,51 @@ function init() {
     addShooter(currentPosition)
   }
 
+  function randomFire() {
+    let missileStartPosition = Math.floor(Math.random() * 20)
+    console.log(missileStartPosition)
+    let missileCurrentPosition = missileStartPosition
+
+
+
+
+    function randomDown() {
+      cells[missileCurrentPosition].classList.remove(charMissileClass)
+      missileCurrentPosition += width
+      cells[missileCurrentPosition].classList.add(charMissileClass)
+
+      if (cells[missileCurrentPosition].classList.contains(charClass)) {
+        cells[missileCurrentPosition].classList.remove(charClass, charMissileClass)
+        cells[missileCurrentPosition].classList.add(charExplosionClass)
+        clearInterval(fireDown)
+        clearInterval(laserTimer)
+        clearInterval(aliensTimer)
+        clearInterval(missileTimer)
+        scoreDisplay.innerHTML = 'GAME OVER'
+
+      }
+
+    }
+
+
+
+    fireDown = setInterval(randomDown, 300)
+  }
+
+  function fireMissiles() {
+    missileTimer = setInterval(randomFire, 4000)
+  }
+
+
+
+
+
+
+
+
+
+
+
   // function addLaser() {
   //   cells[laserCurrentPosition].classList.add(charLaserClass)
   // }
@@ -136,26 +210,50 @@ function init() {
       cells[laserCurrentPosition].classList.remove(charLaserClass)
       laserCurrentPosition -= width
       cells[laserCurrentPosition].classList.add(charLaserClass)
+
+      if (cells[laserCurrentPosition].classList.contains(charAliClass)) {
+        cells[laserCurrentPosition].classList.remove(charLaserClass)
+        cells[laserCurrentPosition].classList.remove(charAliClass)
+        cells[laserCurrentPosition].classList.add(charExplosionClass)
+
+        setTimeout(() => cells[laserCurrentPosition].classList.remove('explosion'), 200)
+        clearInterval(laserTimer)
+
+        const deadAlien = aliens.indexOf(laserCurrentPosition)
+        deadAliens.push(deadAlien)
+        score += 10
+        scoreDisplay.innerHTML = score
+
+
+        console.log(deadAliens)
+      }
+      // const keyCode = event.keyCode
+      // const spaceBar = 32
+
+
+      // alienHit()
     }
 
-    // const keyCode = event.keyCode
-    // const spaceBar = 32
-    if (event.keyCode === 32) {
+    if (event.keyCode === 38) {
       console.log('spacebar')
       laserTimer = setInterval(moveLaserUp, 100)
     }
 
-    alienHit()
-  }
+    function levelComplete() {
 
-  function alienHit() {
-    const laserCurrentPosition = currentPosition
-    if (cells[laserCurrentPosition].classlist.contains(charAliClass, charLaserClass)) {
-      cells[laserCurrentPosition].classList.remove(charAliClass)
-      cells[laserCurrentPosition].classList.remove(charLaserClass)
-      cells[laserCurrentPosition].classList.add(charExplosionClass)
-      return score.innerHTML += 10
+      if (score === 400) {
+        level.innerHTML = 'Level 1 Complete!'
+        clearInterval(aliensTimer)
+        clearInterval(laserTimer)
+      }
+
     }
+
+    levelComplete()
+
+    // function alienHit() {
+    //   const laserCurrentPosition = currentPosition
+
 
 
 
@@ -193,8 +291,12 @@ function init() {
 
   document.addEventListener('keydown', moveShooter)
 
+  startButton.addEventListener('click', start)
+
 
   createGrid()
+  pageLoad()
+  // start()
 }
 
 window.addEventListener('DOMContentLoaded', init)
